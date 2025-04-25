1f9c97e390565dc63173bb1ac1842b4e64a42d12
hwlee.sinsiway <hwlee.sinsiway@gmail.com>
2025-04-25 17:42:05 +0900

    test

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,12 @@ const mysql = require('mysql');
 require('dotenv').config();
 const util = require('util');
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
